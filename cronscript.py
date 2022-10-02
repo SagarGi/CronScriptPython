9ba94a63ddf90e7e4b283031b83d907354c0d19e
sagar <sagargurung1001@gmail.com>
2022-10-02 13:44:41 +0545

    Added First Script

---
+++ cronscript.py
@@ -0,0 +1,5 @@
+# This is the cron job that is used myself for my system written in python script
+
+from notify import notification
+
+notification("This is the Notification Title", message='This message is scheduled from cron')
---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run the this python script with this bash as
+
+# python3 ${HOME}/your-path-to-python-script
